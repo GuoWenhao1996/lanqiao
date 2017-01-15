af44e570d0f912f5435f52801c8f4840c855f0fb
GuoWenhao1996 <1842297753@qq.com>
2017-01-15 11:25:25 +0800

    上传基础练习6-7

--- jclx/basic06/Main.java
+++ jclx/basic06/Main.java
@@ -1,7 +1,26 @@
 package com.gwh.jclx.basic06;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[][] array = new int[n][n];
+		array[0][0] = 1;
+		array[1][0] = 1;
+		array[1][1] = 1;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j <= i; j++) {
+				if (j == 0) {
+					array[i][j] = 1;
+				}
+				if (i > 1 && j > 0) {
+					array[i][j] = array[i - 1][j - 1] + array[i - 1][j];
+				}
+				System.out.print(array[i][j] + " ");
+			}
+			System.out.println();
+		}
 	}
 }
--- jclx/basic07/Main.java
+++ jclx/basic07/Main.java
@@ -2,6 +2,15 @@ package com.gwh.jclx.basic07;
 
 public class Main {
 	public static void main(String[] args) {
-
+		int g = 0;
+		int s = 0;
+		int b = 0;
+		for (int i = 100; i < 1000; i++) {
+			b = i / 100;
+			s = i % 100 / 10;
+			g = i % 10;
+			if (i == b * b * b + s * s * s + g * g * g)
+				System.out.println(i);
+		}
 	}
 }
