c40d8efa0abbe5e1ce9e6925100d76543ed1bfe4
GuoWenhao1996 <1842297753@qq.com>
2017-01-17 10:32:53 +0800

    上传迷宫地图

--- sftg/adv201/Main.java
+++ sftg/adv201/Main.java
@@ -1,7 +1,24 @@
 package com.gwh.sftg.adv201;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		for (int timet = 0; timet < t; timet++) {
+			int n = sc.nextInt();
+			char[][] map = new char[n][n];
+			String mapstr = sc.next();
+			char[] mapchar = mapstr.toCharArray();
+			for (int i = 0; i < n; i++)
+				for (int j = 0; j < n; j++) {
+					map[i][j] = mapchar[i * n + j];
+				}
+			int q = sc.nextInt();
+			for (int timeq = 0; timeq < q; timeq++) {
 
+			}
+		}
 	}
 }
