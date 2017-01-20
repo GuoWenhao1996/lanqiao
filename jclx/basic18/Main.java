416074473733eacb87f533e118d066cde58e9655
GuoWenhao1996 <1842297753@qq.com>
2017-01-20 20:47:29 +0800

    脑袋不清醒。。。

--- jclx/basic18/Main.java
+++ jclx/basic18/Main.java
@@ -13,9 +13,50 @@ public class Main {
 		d11[1] = sc.nextDouble();
 		d12[0] = sc.nextDouble();
 		d12[1] = sc.nextDouble();
+		if (d11[0] > d12[0]) {
+			double temp = d11[0];
+			d11[0] = d12[0];
+			d12[0] = temp;
+		}
+		if (d11[1] > d12[1]) {
+			double temp = d11[1];
+			d11[1] = d12[1];
+			d12[1] = temp;
+		}
 		d21[0] = sc.nextDouble();
 		d21[1] = sc.nextDouble();
-		d21[0] = sc.nextDouble();
-		d21[1] = sc.nextDouble();
+		d22[0] = sc.nextDouble();
+		d22[1] = sc.nextDouble();
+		if (d21[0] > d22[0]) {
+			double temp = d21[0];
+			d21[0] = d22[0];
+			d22[0] = temp;
+		}
+		if (d21[1] > d22[1]) {
+			double temp = d21[1];
+			d21[1] = d22[1];
+			d22[1] = temp;
+		}
+		if ((d12[0] - d11[0]) * (d12[1] - d11[1]) > (d22[0] - d21[0]) * (d22[1] - d21[1])) {
+			double[] d00 = d11;
+			d11 = d21;
+			d21 = d00;
+			d00 = d12;
+			d12 = d22;
+			d22 = d00;
+		}
+		System.out.println(d11[0] + "," + d11[1]);
+		System.out.println(d12[0] + "," + d12[1]);
+		System.out.println(d21[0] + "," + d21[1]);
+		System.out.println(d22[0] + "," + d22[1]);
+
+		if (d11[0] <= d22[0] && d12[0] >= d21[0] && d11[1] <= d22[1] && d12[1] >= d21[1]) {
+			if (d22[1] >= d12[1]) {
+				if()
+			} else {
+
+			}
+		} else
+			System.out.println("0.00");
 	}
 }
