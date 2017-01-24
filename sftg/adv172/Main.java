256317896686898988fbad2f2d7a01ea91b9685a
GuoWenhao1996 <1842297753@qq.com>
2017-01-24 12:58:06 +0800

    身份证。。。

--- sftg/adv172/Main.java
+++ sftg/adv172/Main.java
@@ -8,32 +8,14 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		String[] people = new String[n];
-		int[] birthday = new int[n];
-		int day = 0;
+		long[] birthday = new long[n];
 		for (int i = 0; i < n; i++) {
 			people[i] = sc.next();
-			birthday[i] = Integer.parseInt(people[i].substring(6, 14));
+			birthday[i] = Long.parseLong(people[i]) / 10000 % 100000000 * 100000 + i;
 		}
-		Arrays.sort(people);
 		Arrays.sort(birthday);
 		for (int i = n - 1; i >= 0; i--) {
-			if (i != 0) {
-				if (birthday[i] == birthday[i - 1]) {
-					day++;
-					continue;
-				}
-			}
-			for (int j = n - 1; j >= 0; j--) {
-				if (people[j].substring(6, 14).equals(birthday[i] + "")) {
-					if (day == 0)
-						System.out.println(people[j]);
-					else {
-						for (int k = 0; k <= day; k++)
-							System.out.println(people[j--]);
-						day = 0;
-					}
-				}
-			}
+			System.out.println(people[(int) ((birthday[i]) % 100000)]);
 		}
 	}
 }
