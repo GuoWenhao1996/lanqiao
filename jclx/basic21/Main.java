c7912671d0c44213691779c12828970912d8baba
GuoWenhao1996 <1842297753@qq.com>
2017-01-25 22:20:30 +0800

    上传Sine之舞

--- jclx/basic21/Main.java
+++ jclx/basic21/Main.java
@@ -1,7 +1,32 @@
 package com.gwh.jclx.basic21;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		String[] A = new String[n + 1];
+		for (int j = 1; j <= n; j++) {
+			String An = "";
+			for (int i = 1; i <= j; i++) {
+				An = An + "sin(" + i;
+				if (i != j) {
+					if (i % 2 == 0)
+						An = An + "+";
+					else
+						An = An + "-";
+				}
+			}
+			for (int i = 0; i < j; i++)
+				An = An + ")";
+			A[j] = An;
+		}
+		String Sn = "";
+		for (int i = 0; i < n - 1; i++)
+			Sn = Sn + "(";
+		for (int i = 1; i <= n; i++)
+			Sn = Sn + A[i] + "+" + (n - i + 1) + ")";
+		System.out.println(Sn.substring(0, Sn.length() - 1));
 	}
 }
--- jclx/log.java
+++ jclx/log.java
@@ -25,5 +25,6 @@ public class log {
 		System.out.println("2017-1-22 11:43:44  完成basic19  完美的代价");
 		System.out.println("2017-1-23 17:59:06  完成basic24  龟兔赛跑预测");
 		System.out.println("2017-1-25 21:18:24  完成basic20  数的读法");
+		System.out.println("2017-1-25 22:17:22  完成basic21  Sine之舞");
 	}
 }
