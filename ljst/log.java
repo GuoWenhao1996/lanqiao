c07e92b8a9dee089b5dfcd0167e3b4de62f15546
GuoWenhao1996 <1842297753@qq.com>
2017-03-01 19:24:18 +0800

    完成错误票据（50分）

--- ljst/log.java
+++ ljst/log.java
@@ -7,13 +7,14 @@ public class log {
 		System.out.println("2017-1-24 19:39:35  完成prev15  格子刷油漆(参考代码)");
 		System.out.println("2017-1-24 19:39:35  完成prev23  数字游戏(c++版本参考代码)");
 		System.out.println("2017-1-24 20:17:53  完成prev34  矩阵翻硬币(参考代码)");
-		System.out.println("2017-2-5  10:39:59  完成prev08  买不到的数目(参考代码)");
-		System.out.println("2017-2-5  10:47:30  完成prev02  打印十字图(参考代码)");
-		System.out.println("2017-2-5  11:01:49  完成prev11  横向打印二叉树(参考代码)");
-		System.out.println("2017-2-5  11:12:27  完成prev12  危险系数(参考代码)");
-		System.out.println("2017-2-6  21:30:42  完成prev06  翻硬币");
+		System.out.println("2017-2-05 10:39:59  完成prev08  买不到的数目(参考代码)");
+		System.out.println("2017-2-05 10:47:30  完成prev02  打印十字图(参考代码)");
+		System.out.println("2017-2-05 11:01:49  完成prev11  横向打印二叉树(参考代码)");
+		System.out.println("2017-2-05 11:12:27  完成prev12  危险系数(参考代码)");
+		System.out.println("2017-2-06 21:30:42  完成prev06  翻硬币");
 		System.out.println("2017-2-11 12:49:21  完成prev03  带分数(参考代码)");
 		System.out.println("2017-2-16 11:39:50  完成prev10  幸运数(参考代码)");
 		System.out.println("2017-2-19 17:38:52  完成prev04  剪格子(参考代码)");
+		System.out.println("2017-3-01 19:18:53  完成prev05  错误票据(仅得50分)");
 	}
 }
---
+++ ljst/prev05/Main.java
@@ -0,0 +1,62 @@
+package com.gwh.ljst.prev05;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		String input = sc.nextLine();
+		int[][] num = new int[n][100];
+		int sum = 0;
+		for (int i = 0; i < n; i++) {
+			input = sc.nextLine();
+			input = input.trim() + "#";
+			int begin = 0;
+			int index = 0;
+			for (int j = 0; j < input.length(); j++) {
+				if (input.charAt(j) == ' ') {
+					num[i][index] = Integer.parseInt(input.substring(begin, j));
+					while (true) {
+						if (input.charAt(j + 1) == ' ')
+							j++;
+						else
+							break;
+					}
+					begin = j + 1;
+					index++;
+				} else if (input.charAt(j) == '#') {
+					num[i][index] = Integer.parseInt(input.substring(begin, j));
+					index++;
+				}
+			}
+			sum = sum + index;
+		}
+		int[] allnum = new int[sum];
+		int step = 0;
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < 100; j++) {
+				if (num[i][j] == 0)
+					break;
+				else
+					allnum[step++] = num[i][j];
+			}
+		Arrays.sort(allnum);
+		int que = 0;
+		int duoyu = 0;
+		boolean quebool = true;
+		boolean duoyubool = true;
+		for (int i = 0; i < allnum.length - 1; i++) {
+			if (duoyubool && allnum[i] == allnum[i + 1]) {
+				duoyu = allnum[i];
+				duoyubool = false;
+			}
+			if (quebool && allnum[i] + 1 != allnum[i + 1]) {
+				que = allnum[i] + 1;
+				quebool = false;
+			}
+		}
+		System.out.println(que + " " + duoyu);
+	}
+}
