f379a404dc99e541e0212ee57c884a8e27034c18
GuoWenhao1996 <1842297753@qq.com>
2017-03-05 20:28:34 +0800

    完成 高精度加法

--- jclx/basic29/Main.java
+++ jclx/basic29/Main.java
@@ -1,7 +1,44 @@
 package com.gwh.jclx.basic29;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		int length1 = input.length();
+		int[] a = new int[length1];
+		char[] achar = input.toCharArray();
+		input = sc.nextLine();
+		int length2 = input.length();
+		int[] b = new int[length2];
+		char[] bchar = input.toCharArray();
+		for (int i = 0; i < length1; i++)
+			a[i] = achar[i] - 48;
+		for (int i = 0; i < length2; i++)
+			b[i] = bchar[i] - 48;
+		if (length1 > length2) {
+			for (int i = 0; i < length2; i++) {
+				a[length1 - i - 1] = a[length1 - i - 1] + b[length2 - i - 1];
+				if (a[length1 - i - 1] > 9) {
+					a[length1 - i - 2] += a[length1 - i - 1] / 10;
+					a[length1 - i - 1] = a[length1 - i - 1] % 10;
+				}
+			}
+			for (int i = 0; i < length1; i++) {
+				System.out.print(a[i]);
+			}
+		} else {
+			for (int i = 0; i < length1; i++) {
+				b[length2 - i - 1] = b[length2 - i - 1] + a[length1 - i - 1];
+				if (b[length2 - i - 1] > 9) {
+					b[length2 - i - 2] += b[length2 - i - 1] / 10;
+					b[length2 - i - 1] = b[length2 - i - 1] % 10;
+				}
+			}
+			for (int i = 0; i < length2; i++) {
+				System.out.print(b[i]);
+			}
+		}
 	}
 }
--- jclx/log.java
+++ jclx/log.java
@@ -31,5 +31,6 @@ public class log {
 		System.out.println("2017-2-21 13:22:05  完成basic23  芯片测试");
 		System.out.println("2017-3-03 16:22:59  完成basic25  回形取数");
 		System.out.println("2017-3-04 22:44:38  完成basic26  报时助手");
+		System.out.println("2017-3-05 20:18:33  完成basic29  高精度加法");
 	}
 }
